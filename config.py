afeed1e14b1db747d994efbdc9185c6356166fdb
zomahoundorianne-cpu <zomahoundorianne@gmail.com>
2026-03-11 22:23:07 +0100

    Préparation pour Railway

--- config.py
+++ config.py
@@ -1,15 +1,14 @@
-# config.py
+import os
+
 class Config:
-    # 👇 EMAIL DU CABINET (celui qui reçoit les rappels)
-    CABINET_EMAIL = "zomahoundorianne@gmail.com"  # À remplacer plus tard
+    CABINET_EMAIL = os.environ.get('CABINET_EMAIL', 'zomahoundorianne@gmail.com')
     
-    # 👇 Configuration pour l'envoi des emails (avec Gmail)
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "zomahoundorianne@gmail.com" # Ton email (celui qui envoie)
-    MAIL_PASSWORD = "kcsm csoj vukz kala" # À configurer plus tard
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     
-    # Configuration base de données
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///cedar_o.db'
+    # Railway fournit un volume persistant
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///cedar_o.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
